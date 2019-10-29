d317cd9e10fd23844a48147f400d943114133173
Mia Huong Nguyen <maihuongnt95@gmail.com>
2019-10-29 16:58:11 +0700

    check parse args and load files

--- Engine.h
+++ Engine.h
@@ -34,26 +34,26 @@ template <typename T>
 pair<int, int> load_file(const char* filename, vector<T> &arr);
 
 
-// pair<int, int> load_inputs(string dir, vector<DOUBLE> &h,
-// 				vector<DOUBLE> &hsnham,
-// 				vector<DOUBLE> &VISCOINDX, 
-// 				vector<DOUBLE> &bc_up,
-// 				vector<DOUBLE> &bc_right,
-// 				vector<DOUBLE> &bc_left, 
-// 				vector<DOUBLE> &bc_down, 
-// 				vector<DOUBLE> &CC_u, 
-// 				vector<DOUBLE> &CC_d,
-// 				vector<DOUBLE> &CC_l,
-// 				vector<DOUBLE> &CC_r,
-// 				vector<int> &bienQ);
+pair<int, int> load_inputs(string dir, vector<DOUBLE> &h,
+				vector<DOUBLE> &hsnham,
+				vector<DOUBLE> &VISCOINDX, 
+				vector<DOUBLE> &bc_up,
+				vector<DOUBLE> &bc_right,
+				vector<DOUBLE> &bc_left, 
+				vector<DOUBLE> &bc_down, 
+				vector<DOUBLE> &CC_u, 
+				vector<DOUBLE> &CC_d,
+				vector<DOUBLE> &CC_l,
+				vector<DOUBLE> &CC_r,
+				vector<int> &bienQ);
 
-// // cudaError_t initialize(int device);
+// cudaError_t initialize(int device);
 // void load_initial_condition(string dir, 
 // 							vector<DOUBLE> &u, 
 // 							vector<DOUBLE> &v,
 // 							vector<DOUBLE> &z,
 // 							vector<DOUBLE> &FS,
-// 							vector<int> &khouot);
+							// vector<int> &khouot);
 
 // template <typename T>
 // void device_copy(vector<T> &source, T* des);

e32b17c86c262026e6025d1c71401d632d8e4e10
Mia Huong Nguyen <maihuongnt95@gmail.com>
2019-12-03 22:51:31 +0700

    test Onetimelimit

--- support_funcs.cpp
+++ support_funcs.cpp
@@ -65,7 +65,7 @@ __global__ void Onetime_init( Argument_Pointers *arg, Constant_Coeffs* coeffs){
 	if (h[(i - 1) * width + j] + h[i * width + j] != 0)
 		Ky1[i * width + j] = g * powf((h[(i - 1) * width + j] + h[i * width + j]) * 0.5, -2 * mu_mn) * powf((hsnham[(i - 1) * width + j] + hsnham[i * width + j]) * 0.5, 2);
 
-	printf("Onetime_init Done\n", );
+	printf("Onetime_init Done\n" );
 
 
 }

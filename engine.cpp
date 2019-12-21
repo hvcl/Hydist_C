6c8d16b87fb3208d0a6cbc974d76f72e7b5bac01
Mia Huong Nguyen <maihuongnt95@gmail.com>
2019-12-21 23:27:12 +0700

    testing kernel by kernel in main loop

--- engine.cpp
+++ engine.cpp
@@ -76,8 +76,7 @@ void Hydraulic_Calculation(DOUBLE dT, DOUBLE NANGDAY, Argument_Pointers* d_arg_p
 		grid_shape = (M1, (int) ceil( N / 1024.0), 1);
 
 		cout << grid_shape.x << " " << grid_shape.y << " " << grid_shape.z << endl;
-		cout << "M : " << M << " N: " << N << endl;
-
+		cout << "M : " << M1 << " N: " << N1 << endl;
 
 		UZSolver_calculate_preindex <<<grid_shape, block_shape>>> (start_idx, end_idx, d_arg_ptr, d_arr_ptr, coeffs);
 		synch_and_check();

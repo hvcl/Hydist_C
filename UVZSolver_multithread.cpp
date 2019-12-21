d5978d29e48337d99560dc3b290753cc501b2443
Mia Huong Nguyen <maihuongnt95@gmail.com>
2019-12-21 23:46:30 +0700

    testing kernel by kernel in main loop

--- UVZSolver_multithread.cpp
+++ UVZSolver_multithread.cpp
@@ -815,8 +815,10 @@ UZSolver_calculate_matrix_coeff(int startidx, int endidx, DOUBLE NANGDAY, Argume
     int first = 0; int last = 0;
 
     int seg_no = locate_segment_u(arg->N, arg->M, &bienran1, &bienran2, &first, &last, i, j, arg->dauj, arg->cuoij, arg->mocj, arg->h, NANGDAY);
-    // _calculate_matrix_coeff(true, j, i, arg->N + 2, 2 * (arg->N) + 1, first, last, seg_no, bienran1, bienran2,
-    //                       arg->ubt[j], arg->ubp[j], arg->t_z[last * (arg->M + 3) + j], arg->t_z[first * (arg->M + 3) + j], arg->bienQ[2], arg->bienQ[3], arg->N ,arg,arr);
+    printf("first = %d last = %d\n", first, last );
+
+    _calculate_matrix_coeff(true, j, i, arg->N + 2, 2 * (arg->N) + 1, first, last, seg_no, bienran1, bienran2,
+                          arg->ubt[j], arg->ubp[j], arg->t_z[last * (arg->M + 3) + j], arg->t_z[first * (arg->M + 3) + j], arg->bienQ[2], arg->bienQ[3], arg->N ,arg,arr);
 
 }
 

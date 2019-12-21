c270355710649485339e9225b443f9174df85f67
Mia Huong Nguyen <maihuongnt95@gmail.com>
2019-12-21 23:51:44 +0700

    testing kernel by kernel in main loop

--- UVZSolver_multithread.cpp
+++ UVZSolver_multithread.cpp
@@ -307,9 +307,10 @@ __device__ void _calculate_matrix_coeff(bool isU, int i, int j, int support_arra
     DOUBLE ubt_or_vbd, DOUBLE ubp_or_vbt,  DOUBLE TZ_r, DOUBLE TZ_l, bool dkBienQ_1, bool dkBienQ_2, int dkfr, Argument_Pointers* arg, Array_Pointers *arr){
     //DOUBLE *a1, DOUBLE *b1, DOUBLE *c1, DOUBLE *d1, DOUBLE *a2, DOUBLE *b2, DOUBLE *c2, DOUBLE *d2, DOUBLE* AA, DOUBLE* BB, DOUBLE* CC, DOUBLE* DD){
     
+    printf("i %d, j %d, first %d, last %d\n",i, j, first, last );
+    
 
     if ((first > last) || (j < first) || ( j > last)) return;
-    printf("i %d, j %d, first %d, last %d\n",i, j, first, last );
     return;
 
 

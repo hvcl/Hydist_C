0ee330208beb7a33f6e4916249ca870e32cffb79
Mia Huong Nguyen <maihuongnt95@gmail.com>
2019-12-21 23:51:00 +0700

    testing kernel by kernel in main loop

--- UVZSolver_multithread.cpp
+++ UVZSolver_multithread.cpp
@@ -310,7 +310,8 @@ __device__ void _calculate_matrix_coeff(bool isU, int i, int j, int support_arra
 
     if ((first > last) || (j < first) || ( j > last)) return;
     printf("i %d, j %d, first %d, last %d\n",i, j, first, last );
-    
+    return;
+
 
 
     // int array_width = arg->M + 2;

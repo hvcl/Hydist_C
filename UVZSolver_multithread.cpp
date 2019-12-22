bd41deee14f279bf0ce17cc73c523fb5a3533e91
Mia Huong Nguyen <maihuongnt95@gmail.com>
2019-12-22 16:13:52 +0700

    testing uzsolver calculate matrix

--- UVZSolver_multithread.cpp
+++ UVZSolver_multithread.cpp
@@ -826,7 +826,7 @@ UZSolver_calculate_matrix_coeff(int startidx, int endidx, DOUBLE NANGDAY, Argume
 
     int seg_no = locate_segment_u(arg->N, arg->M, &bienran1, &bienran2, &first, &last, i, j, arg->dauj, arg->cuoij, arg->mocj, arg->h, NANGDAY);
     
-    printf("ubt = %llf, ubp = %llf\n", arg->ubt[j], arg->ubp[j] );
+    // printf("ubt = %llf, ubp = %llf\n", arg->ubt[j], arg->ubp[j] );
     return;
 
     _calculate_matrix_coeff(true, j, i, arg->N + 2, 2 * (arg->N) + 1, first, last, seg_no, bienran1, bienran2,
--- support_funcs.cpp
+++ support_funcs.cpp
@@ -531,6 +531,9 @@ __global__ void preprocess_data(Argument_Pointers* arg, Constant_Coeffs* coeffs)
 
 		for (int i = dauj[M * segment_limit + k]; i <= cuoij[M * segment_limit + k]; i++){
 			hi[i] = powf(hi[i], 2.0 / 3.0) / (sum * dX ) ;//* hsnham[i * width + M]);
+			if (threadIdx.x == 0){
+				printf("up hi[%d] = %llf\n",i, hi[i] );
+			}
 		}
 
 	}
@@ -549,6 +552,9 @@ __global__ void preprocess_data(Argument_Pointers* arg, Constant_Coeffs* coeffs)
 
 		for (int i = dauj[2 * segment_limit + k]; i <= cuoij[2 * segment_limit + k]; i++){
 			hi[i] = powf(hi[i], 2.0 / 3.0) / (sum * dX) ; // * hsnham[i * width + 2]);
+			if (threadIdx.x == 0){
+				printf("down hi[%d] = %llf\n",i, hi[i] );
+			}
 		}
 
 	}
@@ -565,6 +571,9 @@ __global__ void preprocess_data(Argument_Pointers* arg, Constant_Coeffs* coeffs)
 
 		for (int i = daui[2 * segment_limit + k]; i <= cuoii[2 * segment_limit + k]; i++){
 			hi[i] = powf(hi[i], 2.0 / 3.0) / (sum * dX); // * hsnham[2 * width + i]);
+			if (threadIdx.x == 0){
+				printf("left hi[%d] = %llf\n",i, hi[i] );
+			}
 		}
 
 	}
@@ -579,6 +588,9 @@ __global__ void preprocess_data(Argument_Pointers* arg, Constant_Coeffs* coeffs)
 		}
 		for (int i = daui[N * segment_limit + k]; i <= cuoii[N * segment_limit + k]; i++){
 			hi[i] = powf(hi[i], 2.0 / 3.0) / (sum * dX);// * hsnham[N * width + i]);
+			if (threadIdx.x == 0){
+				printf("right hi[%d] = %llf\n",i, hi[i] );
+			}
 		}
 
 	}
